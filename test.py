c75e5d579fdde949777963198a571eb946b4a807
Filip Musial <fmusial@outlook.com>
2023-03-31 11:05:32 -0400

    Added more data files, got image capture working

--- test.py
+++ test.py
@@ -1,18 +1,26 @@
 import pybullet as p
 import math
-import time
 import pybullet_data
+from PIL import Image
+import numpy as np
+
 physics_client = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.81)
 distance = 100000
+image_count = 0
 start_pos = [0, 0, 1]
 start_orientation = p.getQuaternionFromEuler([0, 0, 0])
-img_w, img_h = 120, 80
-p.loadURDF("plane.urdf")
-r = p.loadURDF("r2d2.urdf", start_pos, start_orientation)
+img_w, img_h = 240, 160
+p.loadSDF("data/kitchens/1.sdf", globalScaling=2)
+p.loadURDF("table/table.urdf", [2, 2, 0])
+r = p.loadURDF("racecar/racecar_differential.urdf", start_pos, start_orientation)
+far = 100
+near = 0.02
+
 
 while True:
+    count = 0
     p.stepSimulation()
     robot_position, robot_orientation = p.getBasePositionAndOrientation(r)
 
@@ -32,11 +40,24 @@ while True:
                     )
 
     projection_matrix = p.computeProjectionMatrixFOV(
-                            fov=90, aspect=1.5, nearVal=0.02, farVal=3.5)
+                            fov=90, aspect=1.5, nearVal=near, farVal=far)
 
     imgs = p.getCameraImage(img_w, img_h,
                             view_matrix,
-                            projection_matrix, shadow=True,
+                            projection_matrix,
                             renderer=p.ER_BULLET_HARDWARE_OPENGL)
 
-    time.sleep(1./240.)
+    count = count + 1
+    #if count == 1000
+    rgb = np.reshape(imgs[2], (img_h, img_w, 4))
+    rgb = rgb.astype(np.uint8)
+    depth = np.reshape(imgs[3], [img_h, img_w])
+    depth = far * near / (far - (far - near) * depth)
+    depth = depth.astype(np.uint8)
+#   image = plt.imshow(rgb, interpolation='none', animated=True, label="black")
+#   plt.savefig("rgb_{0}".format(image_count))
+    rgb_image = Image.fromarray(rgb)
+    depth_image = Image.fromarray(depth)
+    rgb_image.save('training_data/rgb_{}.png'.format(image_count))
+    depth_image.save('training_data/depth_{}.png'.format(image_count))
+    image_count = image_count + 1
